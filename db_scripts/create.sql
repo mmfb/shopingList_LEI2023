aaef667f3a67b173c56ccb18cbe4d622ff1c05a6
Miguel Bugalho <miguel.bugalho@gmail.com>
2023-03-20 11:20:41 +0000

    Added the database scripts for the shoplist - Added tables shoplist, product, item and unit - Added a populate with 2 users, 5 products and 4 shoplists with some items Note: the database diagam was not updates

--- db_scripts/create.sql
+++ db_scripts/create.sql
@@ -1,4 +1,4 @@
--- Create the database with the name: myproj
+-- Create the database with the name: shopingList
 -- Then run the create table bellow
 
 create table appuser (
@@ -7,3 +7,48 @@ create table appuser (
     usr_pass varchar(200) not null, 
     usr_token varchar(200),
     primary key (usr_id));
+
+create table shoplist (
+    shl_id serial,
+    shl_usr_id int not null,
+    shl_name varchar(60) not null,
+    shl_created date not null default CURRENT_DATE,
+    shl_due date,
+    primary key (shl_id));
+
+create table product (
+    prd_id serial,
+    prd_name varchar(60) not null,
+    prd_img_url varchar(300),
+    primary key (prd_id));
+
+create table item (
+    it_id serial,
+    it_shl_id int not null,
+    it_prd_id int not null,
+    it_quant decimal(6,3),
+    it_un_id int,
+    primary key (it_id));
+
+create table unit (
+    un_id serial,
+    un_name varchar(40),
+    primary key (un_id));
+
+-- Foreign Keys
+
+alter table shoplist add constraint shoplist_fk_appuser
+            foreign key (shl_usr_id) references appuser(usr_id) 
+			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table item add constraint item_fk_shoplist
+            foreign key (it_shl_id) references shoplist(shl_id) 
+			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table item add constraint item_fk_product
+            foreign key (it_prd_id) references product(prd_id) 
+			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table item add constraint item_fk_unit
+            foreign key (it_un_id) references unit(un_id) 
+			ON DELETE NO ACTION ON UPDATE NO ACTION;
--- db_scripts/populate.sql
+++ db_scripts/populate.sql
@@ -1,1 +1,26 @@
 -- None at the moment,users must be created using the API because of the bcrypt
+
+-- hashed password 123
+INSERT INTO appuser VALUES (1,'me','$2b$10$Wemfac2wY/7RSCdKxuYUL.GV2clfhXC66OL76uCpDFUmpYZ/bGZtW',null),
+(2,'me2','$2b$10$6j2xIDnnxv.TLfBSstbbO.qE7wFTf5envx/uijiFjCP3slsy7EE4K',null);
+
+insert into shoplist(shl_usr_id,shl_name) values
+   (1,'Week shoplist'),
+   (1,'Easter shoplist'),
+   (1,'Party shoplist'),
+   (2,'Monthly shoplist');
+
+insert into product(prd_name,prd_img_url) values 
+('Potato','https://images.pexels.com/photos/2286776/pexels-photo-2286776.jpeg'),
+('Pork','https://images.pexels.com/photos/8251005/pexels-photo-8251005.jpeg'),
+('Milk',null),
+('Eggs','https://images.pexels.com/photos/162712/egg-white-food-protein-162712.jpeg'),
+('Tomatos','https://images.pexels.com/photos/4022083/pexels-photo-4022083.jpeg');
+
+insert into unit (un_name) values ('units'),('kg'),('half dozen'),('liter');
+
+insert into item(it_shl_id,it_prd_id,it_un_id,it_quant) values
+  (1,3,4,3), (1,4,3,1), (2,1,2,2.5), (2,2,2,1.5),(2,5,2,1),
+  (3,4,3,2), (3,3,4,2), (4,4,3,1), (4,2,2,0.7);
+
+
